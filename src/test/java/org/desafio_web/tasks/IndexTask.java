147a93d2e3a92f2fba64170cbe6e77711ef920ef
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2024-06-17 18:39:28 -0300

    Criei outro método para um segundo cadastro.

--- src/test/java/org/desafio_web/tasks/IndexTask.java
+++ src/test/java/org/desafio_web/tasks/IndexTask.java
@@ -6,8 +6,10 @@ import org.openqa.selenium.WebDriver;
 public class IndexTask {
 
     private final IndexAppObjects indexAppObjects;
+    private final WebDriver driver;
 
     public IndexTask (WebDriver driver) {
+        this.driver = driver;
         indexAppObjects = new IndexAppObjects(driver);
     }
 
@@ -18,6 +20,23 @@ public class IndexTask {
         indexAppObjects.getPasswordField().sendKeys("1234567890");
         indexAppObjects.getPasswordConfirmationField().sendKeys("1234567890");
         indexAppObjects.getCreateAccountBalanceToggle().click();
+        indexAppObjects.getRegisterFinalButton();
+        indexAppObjects.getCloseModalButton().click();
+    }
+
+    public void createRegisterOther() {
+        indexAppObjects.getRegisterButton().click();
+        indexAppObjects.getEmailField().clear();
+        indexAppObjects.getEmailField().sendKeys("eugenio@hotmail.com");
+        indexAppObjects.getNameField().clear();
+        indexAppObjects.getNameField().sendKeys("Eugenio");
+        indexAppObjects.getPasswordField().clear();
+        indexAppObjects.getPasswordField().sendKeys("0987654321");
+        indexAppObjects.getPasswordConfirmationField().clear();
+        indexAppObjects.getPasswordConfirmationField().sendKeys("0987654321");
+        indexAppObjects.getCreateAccountBalanceToggle().click();
+        indexAppObjects.getRegisterFinalButton();
+        indexAppObjects.getCloseModalButton().click();
     }
 
 }
