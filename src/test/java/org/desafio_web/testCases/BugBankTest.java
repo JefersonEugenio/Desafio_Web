befedfa6ac5e01a58d8234e43e8882107ab8e2ad
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2024-06-19 16:35:21 -0300

    Adicionei as novas instâncias de importação.

--- src/test/java/org/desafio_web/testCases/BugBankTest.java
+++ src/test/java/org/desafio_web/testCases/BugBankTest.java
@@ -3,8 +3,10 @@ package org.desafio_web.testCases;
 import org.desafio_web.framework.data.EncapsulationData;
 import org.desafio_web.framework.supports.BaseTest;
 import org.desafio_web.framework.webDrivers.DriverManager;
+import org.desafio_web.tasks.HomeTask;
 import org.desafio_web.tasks.RegisterTask;
 import org.desafio_web.tasks.LoginTask;
+import org.desafio_web.tasks.TransferTask;
 import org.junit.jupiter.api.Test;
 
 import static org.desafio_web.framework.data.UseresData.userEugenio;
@@ -13,7 +15,9 @@ import static org.desafio_web.framework.data.UseresData.userJeferson;
 public class BugBankTest extends BaseTest {
 
     private RegisterTask indexTask = new RegisterTask(DriverManager.getDriver());
-    private LoginTask loginTask= new LoginTask(DriverManager.getDriver());
+    private LoginTask loginTask = new LoginTask(DriverManager.getDriver());
+    private HomeTask homeTask = new HomeTask(DriverManager.getDriver());
+    private TransferTask transferTask = new TransferTask(DriverManager.getDriver());
 
     EncapsulationData jeferson = userJeferson();
     EncapsulationData eugenio = userEugenio();
@@ -23,6 +27,9 @@ public class BugBankTest extends BaseTest {
         indexTask.createRegister(jeferson);
         indexTask.createRegister(eugenio);
         loginTask.accessAccount(jeferson);
+        homeTask.transferOtherAccount();
+        transferTask.dataAccount(eugenio);
+        loginTask.accessAccount(eugenio);
     }
 
 }
