623dc90e1399f3537c342a47234cf0a18cd1ff65
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2024-06-19 10:11:39 -0300

    Modifiquei todos os nomes de IndexAppObjects para RegisterAppObjects

--- src/test/java/org/desafio_web/tasks/RegisterTask.java
+++ src/test/java/org/desafio_web/tasks/RegisterTask.java
@@ -1,37 +1,33 @@
 package org.desafio_web.tasks;
 
-import org.desafio_web.appObjects.IndexAppObjects;
+import org.desafio_web.appObjects.RegisterAppObjects;
 import org.desafio_web.appObjects.LoginAppObjects;
 import org.desafio_web.framework.data.EncapsulationData;
 import org.desafio_web.framework.utils.ObjectsUtils;
 import org.desafio_web.framework.webDrivers.DriverManager;
 import org.openqa.selenium.WebDriver;
 
-public class IndexTask {
+public class RegisterTask {
 
     private final WebDriver driver;
-    private final IndexAppObjects indexAppObjects;
+    private final RegisterAppObjects registerAppObjects;
     private final LoginAppObjects loginAppObjects;
 
-    public IndexTask (WebDriver driver) {
+    public RegisterTask(WebDriver driver) {
         this.driver = driver;
         loginAppObjects = new LoginAppObjects(driver);
-        indexAppObjects = new IndexAppObjects(driver);
+        registerAppObjects = new RegisterAppObjects(driver);
     }
 
     public void createRegister(EncapsulationData user) {
         loginAppObjects.getLoginRegisterButton().click();
-        indexAppObjects.getRegisterEmailField().sendKeys(user.getEmail());
-        indexAppObjects.getRegisterNameField().sendKeys(user.getName());
-        indexAppObjects.getRegisterPasswordField().sendKeys(user.getPassword());
-        indexAppObjects.getResgiterPasswordConfirmationField().sendKeys(user.getConfirmPassword());
-        indexAppObjects.getRegisterCreateAccountBalanceToggle().click();
-        indexAppObjects.getRegisterFinalButton();
-        String numberAccount = indexAppObjects.getNumberAccountText().getText();
-        user.setAccount(numberAccount);
-        System.out.println(user.getName() + ", sua conta é: " + user.getAccount());
-        ObjectsUtils.setPropertiesData("dados", user.getName(), user.getAccount());
-        indexAppObjects.getCloseModalButton().click();
+        registerAppObjects.getRegisterEmailField().sendKeys(user.getEmail());
+        registerAppObjects.getRegisterNameField().sendKeys(user.getName());
+        registerAppObjects.getRegisterPasswordField().sendKeys(user.getPassword());
+        registerAppObjects.getResgiterPasswordConfirmationField().sendKeys(user.getConfirmPassword());
+        registerAppObjects.getRegisterCreateAccountBalanceToggle().click();
+        registerAppObjects.getRegisterFinalButton();
+        registerAppObjects.getCloseModalButton().click();
         DriverManager.getDriver().navigate().refresh();
     }
 
