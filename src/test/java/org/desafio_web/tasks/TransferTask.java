ea228f1602c0f3623159787af4193f3ea2eac1b6
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2024-06-19 16:27:02 -0300

    Criei a classe da página de transferência da conta e preenchi os campos para transferência para outra conta

---
+++ src/test/java/org/desafio_web/tasks/TransferTask.java
@@ -0,0 +1,30 @@
+package org.desafio_web.tasks;
+
+import org.desafio_web.appObjects.TransferAppObjects;
+import org.desafio_web.framework.data.EncapsulationData;
+import org.desafio_web.framework.utils.ObjectsUtils;
+import org.openqa.selenium.WebDriver;
+
+public class TransferTask {
+
+    private final WebDriver driver;
+    private final TransferAppObjects transferAppObjects;
+
+    public TransferTask (WebDriver driver) {
+        this.driver = driver;
+        transferAppObjects = new TransferAppObjects(driver);
+    }
+
+    public void dataAccount(EncapsulationData user) {
+        String numberAccount = ObjectsUtils.getPropertiesData("dados", user.getName());
+        String number = numberAccount.split("-")[0];
+        String digit = numberAccount.split("-")[1];
+        transferAppObjects.getNumberAccountField().sendKeys(number);
+        transferAppObjects.getNumberDigitField().sendKeys(digit);
+        transferAppObjects.getValueTransferFiled().sendKeys("200");
+        transferAppObjects.getDescriptionFiled().sendKeys("Doar");
+        transferAppObjects.getTransferNowButton().click();
+        transferAppObjects.getTransferCloseButton().click();
+        transferAppObjects.getTransExitButton().click();
+    }
+}
