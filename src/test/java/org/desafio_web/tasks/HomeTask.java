1a8bb8a6e9f2d3543c537db07628d075c33617d9
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2024-06-19 16:24:59 -0300

    Criei a classe da página principal da conta e clica no botão tranferencia.

---
+++ src/test/java/org/desafio_web/tasks/HomeTask.java
@@ -0,0 +1,19 @@
+package org.desafio_web.tasks;
+
+import org.desafio_web.appObjects.HomeAppObjects;
+import org.openqa.selenium.WebDriver;
+
+public class HomeTask {
+
+    private final WebDriver driver;
+    private final HomeAppObjects homeAppObjects;
+
+    public HomeTask (WebDriver driver) {
+        this.driver = driver;
+        homeAppObjects = new HomeAppObjects(driver);
+    }
+
+    public void transferOtherAccount() {
+        homeAppObjects.getTransferButton().click();
+    }
+}
