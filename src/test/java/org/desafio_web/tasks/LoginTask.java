985f69b705b40e4ca3c62f3444d1dbd4ce68c176
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2024-06-19 10:21:34 -0300

    Criei classes para a parte de login: App e Task.

---
+++ src/test/java/org/desafio_web/tasks/LoginTask.java
@@ -0,0 +1,23 @@
+package org.desafio_web.tasks;
+
+import org.desafio_web.appObjects.LoginAppObjects;
+import org.desafio_web.framework.data.EncapsulationData;
+import org.openqa.selenium.WebDriver;
+
+public class LoginTask {
+
+    private final WebDriver driver;
+    private final LoginAppObjects loginAppObjects;
+
+    public LoginTask (WebDriver driver) {
+        this.driver = driver;
+        loginAppObjects = new LoginAppObjects(driver);
+    }
+
+    public void accessAccount(EncapsulationData user) {
+        loginAppObjects.getLoginEmailField().sendKeys(user.getEmail());
+        loginAppObjects.getLoginPasswordField().sendKeys(user.getPassword());
+        loginAppObjects.getLoginAccessButton().click();
+    }
+
+}
