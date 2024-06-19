4f356c38ea10ef31728d629502283c04a2794ef9
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2024-06-19 16:21:00 -0300

    Criei a classe da página principal da conta e para obter os elementos.

---
+++ src/test/java/org/desafio_web/appObjects/HomeAppObjects.java
@@ -0,0 +1,30 @@
+package org.desafio_web.appObjects;
+
+import org.desafio_web.framework.supports.Wait;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HomeAppObjects {
+
+    private final WebDriver driver;
+    private final Wait wait;
+
+    public HomeAppObjects (WebDriver driver) {
+        this.driver = driver;
+        wait = new Wait(driver);
+    }
+
+    public WebElement getTransferButton() {
+        wait.waitVisibilityElement(By.id("btn-TRANSFERÊNCIA"));
+        return driver.findElement(By.id("btn-TRANSFERÊNCIA"));
+    }
+
+    public WebElement getExtractButton() {
+        return driver.findElement(By.id("btn-EXTRATO"));
+    }
+
+    public WebElement getExitButton() {
+        return driver.findElement(By.id("btnExit"));
+    }
+}
