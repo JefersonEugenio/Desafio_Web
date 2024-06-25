f41c4eadeb0a32765c45d659a8b2c470c9fb3faa
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2024-06-24 21:09:49 -0300

    Alguns não utilizados e removi.

--- src/main/java/org/desafio_web/framework/utils/CreateFolder.java
+++ src/main/java/org/desafio_web/framework/utils/CreateFolder.java
@@ -13,7 +13,6 @@ public class CreateFolder {
         Path createFile = Paths.get(path);
         if (!Files.exists(createFile)) {
             Files.createFile(createFile);
-            System.out.println("Arquivo criado com sucesso!");
         } else {
             System.out.println("Arquivo já existe!");
         }
@@ -24,25 +23,11 @@ public class CreateFolder {
         boolean isDelete = Files.deleteIfExists(directoryDelete);
 
         if (isDelete) {
-            System.out.println("Arquivo apagado com sucesso");
+
         } else {
             System.out.println("Arquivo não encontrado");
         }
 
-    }
-
-    public static void deleteDirectoryRecursively(File file) throws IOException {
-        if (file.isDirectory()) {
-            // Se for um diretório, apagar o conteúdo recursivamente
-            File[] entries = file.listFiles();
-            if (entries != null) {
-                for (File entry : entries) {
-                    deleteDirectoryRecursively(entry);
-                }
-            }
-        }
-        // Finalmente, apagar o próprio arquivo ou diretório
-        Files.delete(file.toPath());
     }
 
     public static void createDirectory(String path){
