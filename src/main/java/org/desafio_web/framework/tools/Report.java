7230e9c21a754853c32f0846df89f24e2a93fcd8
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2024-07-10 17:58:11 -0300

    Removi codigos antigos e adicionei codigos atualizados.

--- src/main/java/org/desafio_web/framework/tools/Report.java
+++ src/main/java/org/desafio_web/framework/tools/Report.java
@@ -2,7 +2,7 @@ package org.desafio_web.framework.tools;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 import org.desafio_web.framework.utils.CreateFolder;
 import org.openqa.selenium.WebDriver;
@@ -11,29 +11,29 @@ import java.io.File;
 
 public class Report {
 
-    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
-    private static final String CAMINHO_REPORT = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "report";
+    private static final ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+    private static final String CAMINHO_REPORT = System.getProperty("user.dir")
+            + File.separator + "src" + File.separator + "main"
+            + File.separator + "resources" + File.separator + "report";
     public static WebDriver driver;
-    public static ExtentHtmlReporter htmlReporter;
+    public static ExtentSparkReporter sparkReporter;
     public static ExtentReports extentReports;
     public static ExtentTest extentTest;
 
     public static void configurarExtentReport(){
         CreateFolder.createDirectory(CAMINHO_REPORT);
 
-        htmlReporter = new ExtentHtmlReporter(CAMINHO_REPORT + File.separator + "ReportTest.html");
+        sparkReporter = new ExtentSparkReporter(CAMINHO_REPORT + File.separator + "ReportTestSpark.html");
 
-        htmlReporter.config().setDocumentTitle("Automação de web");
-        htmlReporter.config().setReportName("Automação de teste");
-        htmlReporter.config().setEncoding("UTF-8");
-        htmlReporter.config().setTheme(Theme.DARK);
-        htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
-        htmlReporter.config().setJS("$('.brand-logo').html('<img src=\\\"https://i.imgur.com/qDYwkD2.png\\\" class=\\\"db-logo\\\"/>');");
-        htmlReporter.config().setCSS(".nav-wrapper { background-color: #1E317A !important; } .brand-logo { background-color: #1E317A !important; padding: 0 10px 0 0 !important; margin: 0 !important; position: absolute !important } .report-name { margin-left: 80px !important } .blue.darken-3 { background-color: #1E317A !important; color: #FFF !important;  }");
+        sparkReporter.config().setDocumentTitle("Automação de web");
+        sparkReporter.config().setReportName("Automação de teste");
+        sparkReporter.config().setEncoding("UTF-8");
+        sparkReporter.config().setTheme(Theme.DARK);
+        sparkReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
 
         extentReports = new ExtentReports();
 
-        extentReports.attachReporter(htmlReporter);
+        extentReports.attachReporter(sparkReporter);
     }
 
     public static void createTest(String testName){
