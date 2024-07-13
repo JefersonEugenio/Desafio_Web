378e1387fdb06b14ab06e5088b38e7448eb3a29a
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2024-07-13 12:21:52 -0300

    Removi import ObjectsUtils e adicionei import CreateCsv, e alterei algumas sintaxes.

--- src/main/java/org/desafio_web/tasks/ExtractTask.java
+++ src/main/java/org/desafio_web/tasks/ExtractTask.java
@@ -4,8 +4,10 @@ import com.aventstack.extentreports.Status;
 import org.desafio_web.appObjects.ExtractAppObjects;
 import org.desafio_web.framework.data.EncapsulationData;
 import org.desafio_web.framework.tools.Screenshot;
-import org.desafio_web.framework.utils.ObjectsUtils;
+import org.desafio_web.framework.utils.CreateCsv;
 import org.openqa.selenium.WebDriver;
+
+import java.io.File;
 
 import static org.desafio_web.framework.tools.Report.extentTest;
 
@@ -20,13 +22,12 @@ public class ExtractTask {
     }
 
     public void validateExtract(EncapsulationData user) throws IllegalAccessException {
+        File pasta = new File(CreateCsv.PATH_CSV + "transfer.csv");
         String value = extractAppObjects.getBalanceText().getText().replaceAll("[^0-9.,]", "");
         user.setBalance(value);
-        if (ObjectsUtils.getPropertiesData("dados", "Value_transfer") == null) {
-            ObjectsUtils.setPropertiesData("dados", "Saldo_"+user.getName(), user.getBalance());
+        if (!pasta.isFile()) {
             extentTest.log(Status.INFO, "Verificar o valor do saldo "+ user.getName()+ " é " + user.getBalance());
         } else {
-            ObjectsUtils.setPropertiesData("dados", "Saldo_"+user.getName(), user.getBalance());
             extentTest.log(Status.PASS, "Verificar o valor do saldo "+ user.getName()+ " é " + user.getBalance(), Screenshot.screenshotBase64(driver));
         }
         extractAppObjects.getExitButton().click();
