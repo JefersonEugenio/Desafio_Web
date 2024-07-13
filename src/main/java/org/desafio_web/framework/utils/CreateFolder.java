0881de7fd1b55798d3893fafb1c9c5967d2d13d9
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2024-07-13 12:19:52 -0300

    Removi dois metodos não utilizados e criei um metodo para apagar a pasta e os arquivos dentro dela.

--- src/main/java/org/desafio_web/framework/utils/CreateFolder.java
+++ src/main/java/org/desafio_web/framework/utils/CreateFolder.java
@@ -3,38 +3,27 @@ package org.desafio_web.framework.utils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class CreateFolder {
-
-    public static void createFolderFile(String path) throws IOException {
-
-        Path createFile = Paths.get(path);
-        if (!Files.exists(createFile)) {
-            Files.createFile(createFile);
-        } else {
-            System.out.println("Arquivo já existe!");
-        }
-    }
-
-    public static void deleteFile(String path) throws IOException {
-        Path directoryDelete = Paths.get(path);
-        boolean isDelete = Files.deleteIfExists(directoryDelete);
-
-        if (isDelete) {
-
-        } else {
-            System.out.println("Arquivo não encontrado");
-        }
-
-    }
 
     public static void createDirectory(String path){
         File directory = new File(path);
         if (!directory.exists()) {
             directory.mkdir();
+            System.out.println("A pasta foi criada!");
         }
+    }
+
+    public static void deleteDirectoryRecursively(File file) throws IOException {
+        if (file.isDirectory()) {
+            File[] entries = file.listFiles();
+            if (entries != null) {
+                for (File entry : entries) {
+                    deleteDirectoryRecursively(entry);
+                }
+            }
+        }
+        Files.delete(file.toPath());
     }
 
 }
